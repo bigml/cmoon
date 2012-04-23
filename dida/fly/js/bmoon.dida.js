fa5ec5ad4f8d99c5c9a7c6255f6c88aa6cad295d
bigml <bigmaliang@gmail.com>
2012-04-23 10:02:10 +0800

    Revert "dida - reload page on loginCheck()"
    This reverts commit 01ecad70ac05b8f8045a1623541d67730ecfa2c9.

--- dida/fly/js/bmoon.dida.js
+++ dida/fly/js/bmoon.dida.js
@@ -274,6 +274,7 @@ bmoon.dida = {
                 return;
             }
             o.loginCheck();
+            o.reloadAfterLogin && setTimeout(function() {location.href = o.loginref || location.href;}, 1000);
         });
     },
     
@@ -307,18 +308,12 @@ bmoon.dida = {
         
         if (o.c_mnick != null) {
             o.loginoverlay.close();
-
+            o.mnick.text(o.c_mnick);
             if (o.c_mid) o.mhome.attr('href', '/member/home?mid='+o.c_mid);
             else o.mhome.attr('href', '/member/home');
-            o.mnick.text(o.c_mnick);
             o.guest.hide();
             o.member.show();
             o.loginmname.val(o.c_mname);
-
-            o.reloadAfterLogin && setTimeout(function() {
-                location.href = o.loginref || location.href;
-            }, 1000);
-
             return true;
         } else {
             o.member.hide();
