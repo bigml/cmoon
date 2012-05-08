bd5e31bcda71dfaf5e31bda91560169276280d9c
bigml <bigmaliang@gmail.com>
2012-05-08 15:06:12 +0800

    dida - spd filter(just get need city)

--- dida/fly/js/admin/spd/58.js
+++ dida/fly/js/admin/spd/58.js
@@ -196,11 +196,73 @@ bmoon.spd58 = {
         var o = bmoon.spd58.init();
 
         var cs = $('#clist a'),
-        pos = 0;
+        pos = 0,
+        dcities = {
+            "ankang":"安康", "am":"澳门", "aks":"阿克苏", "ale":"阿拉尔", "al":"阿里",
+            "ab":"阿坝", "alsm":"阿拉善盟", "anshun":"安顺", "baoji":"宝鸡",
+            "bc":"白城", "bozhou":"亳州", "bygl":"巴音郭楞", "changdu":"昌都",
+            "chongzuo":"崇左", "diqing":"迪庆", "dx":"定西", "dxal":"大兴安岭",
+            "dafeng":"大丰", "fcg":"防城港", "guyuan":"固原", "gt":"馆陶",
+            "ganzi":"甘孜", "guoluo":"果洛", "gn":"甘南", "hlr":" 海拉尔",
+            "ht":"和田", "huangnan":"黄南", "hx":"海西", "hainan":"海南",
+            "haidong":"海东", "haibei":"海北", "heihe":"黑河", "hexian":"和县",
+            "hq":"霍邱", "jinchang":"金昌", "jyg":"嘉峪关", "jq":"酒泉",
+            "kzls":"克孜勒苏", "kl":"垦利", "linxia":"临夏", "linyixian":"临猗",
+            "lincang":"临沧", "linzhi":"林芝", "ln":"陇南", "lb":"来宾",
+            "lps":"六盘水", "mg":"明港", "nujiang":"怒江", "nq":"那曲", "pe":"普洱",
+            "qinzhou":"钦州", "qdn":"黔东南", "qn":"黔南", "qxn":"黔西南",
+            "qingxu":"清徐", "rkz":"日喀则", "sw":"汕尾", "sn":"山南", "sl":"商洛",
+            "szs":"石嘴山", "snj":"神农架", "tw":"台湾", "tmsk":"图木舒克", "tc":"铜川",
+            "tr":"铜仁", "taishan":"台山", "tongcheng":"桐城", "wfd":"瓦房店",
+            "wjq":"五家渠", "wzs":"五指山", "wuyishan":"武夷山", "hk":"香港",
+            "yuzhou":"禹州", "yanling":"鄢陵", "ys":"玉树", "yf":"云浮", "yxx":"永新",
+            "zd":"正定", "zhangqiu":"章丘", "zx":"赵县", "zw":"中卫",
+            "zhangye":"张掖", "zhangbei":"张北", "pld":"庄河"
+        },
+        zcities = {
+            "bs":"保山", "betl":"博尔塔拉", "baishan":"白山", "by":"白银",
+            "baise":"百色", "bh":"北海", "bijie":"毕节", "chenzhou":"郴州",
+            "chifeng":"赤峰", "cx":"楚雄", "changji":"昌吉", "changge":"长葛",
+            "cy":"朝阳", "ch":"巢湖", "chizhou":"池州", "chuzhou":"滁州",
+            "chaozhou":"潮州", "dali":"大理", "dingzhou":"定州", "dh":"德宏",
+            "ez":"鄂州", "fushun":"抚顺", "fy":"阜阳", "gg":"贵港", "ganzhou":"赣州",
+            "ga":"广安", "hh":"怀化", "hc":"河池", "hami":"哈密", "honghe":"红河",
+            "hegang":"鹤岗", "hn":"淮南", "heyuan":"河源", "hezhou":"贺州", "jy":"揭阳",
+            "ja":"吉安", "jz":"晋中", "jinzhou":"锦州", "jdz":"景德镇", "jiyuan":"济源",
+            "klmy":"克拉玛依", "kel":"库尔勒", "lvliang":"吕梁", "liaoyang":"辽阳",
+            "liaoyuan":"辽源", "luohe":"漯河", "la":"六安", "lw":"莱芜", "mdj":"牡丹江",
+            "mz":"梅州", "scnj":"内江", "np":"南平", "pl":"平凉", "panzhihua":"攀枝花",
+            "pt":"莆田", "qqhr":"齐齐哈尔", "quzhou":"衢州", "qingyuan":"清远",
+            "qj":"曲靖", "qingyang":"庆阳", "qth":"七台河", "qianjiang":"潜江",
+            "st":"汕头", "sm":"三明", "sg":"韶关", "sq":"商丘", "shuyang":"沭阳",
+            "suihua":"绥化", "sanya":"三亚", "sd":"顺德", "songyuan":"松原",
+            "smx":"三门峡", "suizhou":"随州", "shuozhou":"朔州", "sr":"上饶",
+            "sp":"四平", "sys":"双鸭山", "tlf":"吐鲁番", "tongliao":"通辽", "tm":"天门",
+            "wuzhou":"梧州", "ws":"文山", "wuhai":"乌海", "wuzhong":"吴忠",
+            "wlcb":"乌兰察布", "wuwei":"武威", "xiangtan":"湘潭", "xf":"襄阳",
+            "xx":"新乡", "xt":"邢台", "xinzhou":"忻州", "xl":"锡林郭勒盟",
+            "xianyang":"咸阳", "xy":"信阳", "xam":"兴安盟", "xinyu":"新余",
+            "xiangxi":"湘西", "xianning":"咸宁", "yingtan":"鹰潭", "yx":"玉溪",
+            "yk":"营口", "yl":"榆林", "yj":"阳江", "yangchun":"阳春", "yq":"阳泉",
+            "ya":"雅安", "yich":"伊春", "zhuzhou":"株洲", "zq":"肇庆", "zj":"镇江",
+            "zmd":"驻马店", "zjj":"张家界", "zy":"资阳", "zunyi":"遵义",
+            "zhoushan":"舟山", "zc":"诸城", "zt":"昭通"
+        };
 
+        o.dcities = dcities;
+        o.zcities = zcities;
+        
         function get() {
-            for (var i = 0; i < 5 && pos < cs.length; i++, pos++) {
-                window.open($(cs[pos]).attr('href'));
+            for (var i = 0; i < 5 && pos < cs.length; pos++) {
+                var href = $(cs[pos]).attr('href'),
+                c = href.match(/http:\/\/(.*).58.com/)[1];
+
+                if (dcities[c] || zcities[c]) {
+                    console.log(c + '暂不获取');
+                } else {
+                    window.open(href);
+                    i++;
+                }
             }
             setTimeout(get, 10*60*1000);
         }
