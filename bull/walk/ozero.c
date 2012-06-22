b0f2ba21745194e7643ce43f8e4e116296d5bc06
bigml <bigmaliang@gmail.com>
2012-06-22 19:03:00 +0800

    bull - fix

--- bull/walk/ozero.c
+++ bull/walk/ozero.c
@@ -26,14 +26,14 @@ NEOERR* zero_image_data_add(CGI *cgi, HASH *dbh, HASH *evth, session_t *ses)
     int ftype;
 	NEOERR *err;
     
-    err = mimg_accept_and_zoomout(cgi, "upfile", ROOT_IMG, fname, &ftype, 900, 0);
+    err = mimg_accept_and_zoomout(cgi, "upfile", ROOT_IMG, fname, &ftype, 620, 0);
 	if (err != STATUS_OK) return nerr_pass(err);
     
     char tok[3] = {0}; strncpy(tok, fname, 2);
 
     hdf_set_valuef(cgi->hdf, PRE_OUTPUT".imageurl=%s%s/%s.%s",
                    URL_IMG, tok, fname, mimg_type_int2str(ftype));
-    hdf_set_valuef(cgi->hdf, PRE_OUTPUT".imageurl_zoom=%s900x0/%s/%s.%s",
+    hdf_set_valuef(cgi->hdf, PRE_OUTPUT".imageurl_zoom=%s620x0/%s/%s.%s",
                    URL_IMG, tok, fname, mimg_type_int2str(ftype));
     hdf_set_valuef(cgi->hdf, PRE_OUTPUT".imagename=%s.%s",
                    fname, mimg_type_int2str(ftype));
